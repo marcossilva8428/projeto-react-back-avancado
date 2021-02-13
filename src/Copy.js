6c8cd91cd3dad0b8389ce6b247b238d6ef196d62
Marcos dos Anjos Bernardes da Silva Junior <marcossilva8428@gmail.com>
2021-02-13 03:07:22 -0300

    projeto-react-back-avancado

---
+++ src/Copy.js
@@ -0,0 +1,12 @@
+  
+import React from 'react';
+
+const Copy = (props) => {
+   return (
+       <div>
+           <h1>Marcos dos Anjos Bernardes da Silva Junior - Recode Pro {props.ano}</h1>
+       </div>
+   )
+}
+
+export default Copy;
